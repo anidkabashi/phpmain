9b72b869f3a143a3a3fb94bf0efc6a04092d8aeb
anidkabashi <anidkabashi@gmail.com>
2025-05-21 03:14:50 -0700

    .

---
+++ lesson5/tow-dimensional_array.php
@@ -0,0 +1,12 @@
+<?php
+    $dogs = array(
+        array("Husky", "Siberia", 15),
+        array("Bulldog", "England", 10),
+        array("Chihuahua", "Mexico", 20)
+    );
+
+    echo $dogs[0][0]. ": Origion:". $dogs[0][1], "Life Span:". $dogs[0][2]. "<br>";
+    echo $dogs[1][0]. ": Origion:". $dogs[1][1], "Life Span:". $dogs[1][2]. "<br>";
+    echo $dogs[2][0]. ": Origion:". $dogs[2][1], "Life Span:". $dogs[2][2]. "<br>";
+
+?>
